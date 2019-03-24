04a6472a5e67d3eda216ed0366f9c43f42fd03b4
Kalinowski.Mikołaj <209347@edu.p.lodz.pl>
2019-03-24 21:14:15 +0100

    testy_v1 - 2. Poprawki do testu z pierwszego commita. Dodanie poprawnego testu i pierwsze zmiany w kodzie BinarySeach.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,8 +8,10 @@ import static org.junit.Assert.*;
 public class BinarySearchTest {
 
     private SearchResult searchResult;
+    private final int ZERO = 0;
     private final int ONE = 1;
     private final int[] SEQ_ONE = { 1 };
+    private final int[] SEQ_TWO = { 2 };
 
     @Before
     public void setup() {
@@ -17,8 +19,21 @@ public class BinarySearchTest {
     }
 
     @Test
-    public void searchShouldReturnOneIfSeqLengthEqualsOneAndKeyIsInSeq() {
+    public void searchShouldReturnTrueIfSeqLengthEqualsOneAndKeyIsInSeq() {
         searchResult = BinarySearch.search(ONE, SEQ_ONE);
         assertEquals(true, searchResult.isFound());
+    }
+
+    @Test
+    public void searchPositionShouldBeZeroIfSeqLegnthEqualsOneAndKeyIsInSeq() {
+        searchResult = BinarySearch.search(ONE, SEQ_ONE);
+        assertEquals(ZERO, searchResult.getPosition());
+        assertEquals(ZERO, SEQ_ONE[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void searchShouldReturnFalseIfSeqLengthEqualsOneAndKeyIsInSeq() {
+        searchResult = BinarySearch.search(ONE, SEQ_TWO);
+        assertEquals(false, searchResult.isFound());
     }
 }
